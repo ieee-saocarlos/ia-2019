a3fb1a140e28d1ca8d2b7d34d54937a37ab2f8aa
Eric Kurachi <erictkurachi@gmail.com>
2019-10-03 20:30:20 -0300

    Movimento da plataforma e detruicao dos tijolos

--- Grupo Eric/tijolos.py
+++ Grupo Eric/tijolos.py
@@ -1,4 +1,16 @@
 import pygame
 
-def desenhar(x, y, vermelho, screen):
-    pygame.draw.rect(screen, vermelho, [300, 300, 50, 15])
+
+def draw(x, y, red, screen, wall, ball_size):
+    i = 0
+    vertexes = [[x, y], [x + ball_size[0], y], [x, y + ball_size[1]], [x + ball_size[0], y + ball_size[1]]]
+
+    for brick in wall:
+        for vertex in vertexes:
+            pygame.draw.rect(screen, red, [brick[0], brick[1], 50, 15])
+            if brick[0] < vertex[0] < brick[0] + 55 and brick[1] < vertex[1] < brick[1] + 15:
+                if abs(vertex[0] - brick[0] < vertex[1] - brick[1]):
+                    return i, 'x'
+                else:
+                    return i, 'y'
+        i += 1
