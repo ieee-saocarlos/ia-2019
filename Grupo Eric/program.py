21bf48ef5c0192ec5bdd4718ffa5804dd24d8de7
Eric Kurachi <erictkurachi@gmail.com>
2019-10-04 03:19:23 -0300

    restore file

--- Grupo Eric/program.py
+++ Grupo Eric/program.py
@@ -1,54 +1,28 @@
 import pygame
 import sys
-import objects
-import random
-
 
 pygame.init()
 clock = pygame.time.Clock()
 
-# Colors
 white = (255, 255, 255)
 black = (0, 0, 0)
-orange = (255, 126, 0)
-red = (255, 0, 0)
-azul = (0, 0, 255)
-verde = (0, 255, 0)
-purple = (255, 0, 255)
 
-size = w, h = 1000, 600
+size = width, height = 600, 600
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('NPC Breakout')
 
-#ball_dir = [random.randint(-1, 2), random.randint(-1, 2)]
-ball_dir = [0, -1]
-
 ball_size = [10, 10]
-ball_speed = 3
-ball_pos = [w/2, h/2 + 100]
-ball_vel = [x * ball_speed for x in ball_dir]
-print(ball_vel)
-
-plat_pos = [w/2, h-25]
-plat_mov = 0
-speed = 7
-
-wall = []
-
-points = 0
-level = 0
-
+ball_pos = [320, 180]
+ball_vel = [5, -5]
 
 def text(string):
     font = pygame.font.SysFont("arial", 20)
-    label = font.render(string, True, white)
+    label = font.render(string, True, black)
     return label
 
-
-def image(window, image_name, x, y):
-    image_v = pygame.image.load(image_name).convert_alpha()
-    window.blit(image_v, (x, y))
-
+def image(window, imageName, x, y):
+    image = pygame.image.load(imageName).convert_alpha()
+    window.blit(image, (x, y))
 
 while True:
     screen.fill(black)
@@ -56,63 +30,17 @@ while True:
     ball_pos[0] = ball_pos[0] + ball_vel[0]
     ball_pos[1] = ball_pos[1] + ball_vel[1]
 
-    if ball_pos[0] < 0 or ball_pos[0] > size[0] - ball_size[0]:
+    if ball_pos[0] == 0 or ball_pos[0] == size[0] - ball_size[0]:
         ball_vel[0] = ball_vel[0] * -1
-    elif ball_pos[1] < 0:
+    if ball_pos[1] == 0 or ball_pos[1] == size[1] - ball_size[1]:
         ball_vel[1] = ball_vel[1] * -1
-    elif ball_pos[1] > size[1] - ball_size[1]:
-        ball_vel = [0, 0]
 
-    if not wall:
-        level += 1
-        # Coordenadas dos tijolos
-        wall = []
-        for column in range(14):
-            for row in range(9):
-                if row is not 0:
-                    wall.append([w / 2 - 25 + row * 55, h / 2 - column * 20])
-                wall.append([w / 2 - 25 - row * 55, h / 2 - column * 20])
-
-    plat_pos[0] += plat_mov
-    ric = objects.plat(ball_pos[0], ball_pos[1], purple, screen, plat_pos, ball_size, ball_vel)
-    if ric is not None:
-        ball_vel[0] = ric[1]
-        if ric[0] == 'x':
-            ball_vel[0] *= -1
-        else:
-            ball_vel[1] *= -1
-
-    col = objects.bricks(ball_pos[0], ball_pos[1], red, screen, wall, ball_size, ball_vel)
-    if col is not None:
-        points += 1
-        # Apaga o tijolo em que a bola colidiu
-        wall.pop(col[0])
-        if col[1] == 'x':
-            ball_vel[0] *= -1
-        else:
-            ball_vel[1] *= -1
-    objects.bricks(ball_pos[0], ball_pos[1], red, screen, wall, ball_size, ball_vel)
-
-    # draw the ball
     pygame.draw.rect(screen, white, [ball_pos[0], ball_pos[1], ball_size[0], ball_size[1]])
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                plat_mov = -speed
-            if event.key == pygame.K_RIGHT:
-                plat_mov = speed
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                plat_mov = 0
-
-    screen.blit(text('score: ' + str(points)), [10, 10])
-    screen.blit(text('level: ' + str(level)), [w - 80, 10])
-
-    clock.tick(60)
+    clock.tick(30)
     pygame.display.update()
 
